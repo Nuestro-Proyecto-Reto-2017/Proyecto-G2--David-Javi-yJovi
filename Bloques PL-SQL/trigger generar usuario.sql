b676debda0c38a1f066e5012b4aaa08efa0a426b
Unknown <dvidsen@gmail.com>
2017-05-08 12:38:12 +0200

    descartes

--- Bloques PL-SQL/trigger generar usuario.sql
+++
@@ -1,8 +0,0 @@
-create or replace trigger generar_user
-after insert on trabajador for each row
-declare 
-  begin   
-  insert into usuario(usuario,contraseña,Trabajador_dni) values( :new.dni, :new.nombre, :new.dni); 
-end;
-
-  
